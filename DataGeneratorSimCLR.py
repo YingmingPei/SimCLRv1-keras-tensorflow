394ace9a05a56061f510088484e106d6394a16e3
YingmingPei <peiyingming@outlook.com>
2025-03-16 12:42:54 -0700

    Update DataGeneratorSimCLR.py

--- DataGeneratorSimCLR.py
+++ DataGeneratorSimCLR.py
@@ -115,4 +115,5 @@ class DataGeneratorSimCLR(data_utils.Sequence):
 
         # [None] is used to silence warning
         # https://stackoverflow.com/questions/59317919/warningtensorflowsample-weight-modes-were-coerced-from-to
-        return list(X), y, [None]
+        # return list(X), y, [None]
+        return list(X), y
